cf206610e9d59f28e31ca6f64a98085849d0bbfd
Jawnathan <jonathan@jonathanschimpf.com>
2020-09-07 21:18:12 -0400

    rough draft commit

---
+++ questionsandanswers.js
@@ -0,0 +1,40 @@
+
+// ***** ARRAY OF OBJECTS FEATURING QUIZ QUESTIONS, POTENTIAL ANSWERS, AND THE CORRECT ANSWER ******
+
+var arrayOfQuestions = [
+    {
+        quizQuestion: "Inside which HTML element do we place the JavaScript?",
+        potentialAnswers: ["<script>", "<javascript>", "<scripting", "<js>"],
+        correctAnswer: "<js>"
+    },
+    {
+        quizQuestion: "What event occurs when a user clicks on an HTML element?",
+        potentialAnswers: ["onclick", "onmouseover", "onchange", "onmouseclick"],
+        correctAnswer: "onclick"
+    },
+    {
+        quizQuestion: "Which operator is used to assign a value to a variable?",
+        potentialAnswers: ["*", "=", "+", "-"],
+        correctAnswer: "="
+    },
+    {
+        quizQuestion: "How can you detect the client's browser name?",
+        potentialAnswers: ["browser.name", "client.navName", "navigator.appName", "browser.appName"],
+        correctAnswer: "navigator.appName"
+    },
+    {
+        quizQuestion: "How do you declare a JavaScript variable?",
+        potentialAnswers: ["v variableName;", "var variableName;", "variable variableName;", "var = variableName"],
+        correctAnswer: "var variableName;"
+    },
+    {
+        quizQuestion: "How do you round the number 25.25, to the nearest integer?",
+        potentialAnswers: ["Math.rnd(25.25)", "round(25.25)", "rnd(25.25)", "Math.round(25.25)"],
+        correctAnswer: "Math.round(25.25)"
+    },
+    {
+        quizQuestion: "How do you call a function named myFunction?",
+        potentialAnswers: ["myFunction()", "call myFunction()", "call function myFunction()", "function myFunction"],
+        correctAnswer: "myFunction()"
+    }
+]
---
+++ script.js
@@ -0,0 +1,145 @@
+// ****** DECLARATION OF VARIABLES ******
+
+var quizBeginsButton = document.querySelector("#quizBeginsButton");
+var highScoresArchive = document.querySelector("#highScoresArchive");
+var highScoresClick = document.querySelector("#highScoresClick");
+var highScoresList = document.querySelector(".highScoresList");
+var timeLeft = document.querySelector("#timeLeft");
+var welcomeSection = document.querySelector(".welcomeSection");
+var questionSection = document.querySelector(".questionSection");
+var presentQuestion = document.querySelector("#presentQuestion");
+var endQuizGreeting = document.querySelector("#endQuizGreeting");
+var initialsSection = document.querySelector(".initialsSection");
+var initialsInput = document.querySelector("#initialsInput");
+var submitButtonClick = document.querySelector(".submitButtonClick");
+//
+var choiceButtons = document.querySelector(".choiceButtons");
+var firstChoice = document.querySelector("#firstChoice");
+var secondChoice = document.querySelector("#secondChoice");
+var thirdChoice = document.querySelector("#thirdChoice");
+var fourthChoice = document.querySelector("#fourthChoice");
+var timeRemaining = 90;
+var currentQuestion = 0;
+
+// functionality for the high scores archive when clicked.
+// archive is hidden until clicked, then displays results.
+
+highScoresClick.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (highScoresList.style.display === "none") {
+        highScoresList.style.display = "block";
+        highScoresArchive.style.display = "block";
+    }
+    else {
+        highScoresList.style.display = "none";
+        highScoresArchive.style.display = "none";
+    }
+})
+
+// user score information is archived on the page along with intials
+// via storePlayerInfo function and JSON.parse feature
+
+function storePlayerInfo() {
+    var usersInitials = localStorage.getItem("initials");
+    var usersTime = localStorage.getItem("time");
+
+    //JSON.parse removes local storage information and places it on the page
+
+    var parse = document.createElement("parse");
+    parse.innerHTML = JSON.parse(usersInitials) + " : " + usersTime;
+    highScoresArchive.append(parse);
+}
+
+// here is where the time function exists, it is initially invisible. 
+// it is activated when the user clicks start and calls un the startTimer function.
+// the timer beging at 90 seconds (timeRemaining), and counts down backwards.
+
+function startTimer() {
+    timeLeft.style.visibility = "visible";
+
+    var timerInterval = setInterval(function () {
+        timeRemaining--;
+        timeLeft.textContent = "Timer: " + timeRemaining;
+
+        if ((currentQuestion === arrayOfQuestions.length)) {
+            clearInterval(timerInterval);
+            var usersTime = timeRemaining;
+            endQuizGreeting.textContent = ("You completed the quiz with " + usersTime + " seconds left!");
+        }
+    }, 1000);
+}
+
+// here is wheere the submit button functionality exists (submitButtonClick)..
+// once the user enters their intitals and clicks submit the score ititials are archived.
+// this allows visibility of the start screen again after submission (welcomeSection).
+
+submitButtonClick.addEventListener("click", function (event) {
+    event.preventDefault();
+    var usersTime = timeRemaining;
+    var usersInitials = initialsInput.value;
+
+    // users score and intitials added here via JSON stringify.
+
+    localStorage.setItem("initials", JSON.stringify(usersInitials));
+    localStorage.setItem("time", JSON.stringify(usersTime + " seconds"));
+    storePlayerInfo();
+    initialsSection.style.display = "none";
+    welcomeSection.style.display = "block";
+    timeLeft.style.visibility = "hidden";
+    timeRemaining = 90;
+    timeLeft.textContent = "Timer: 90";
+
+})
+
+// ****** QUIZ CONTENT ******
+
+// when the start button (quizBeginsButton) is clicked it calls on the startQuiz function
+
+quizBeginsButton.addEventListener("click", startQuiz);
+
+// this hides the welcomeSection menu page section and reveals the questions section (both within index.html)
+// questions are presented while starting the timer by calling on the startTimer and askQuestions functions.
+// the timer begins to count down as a result, and the user is presented with multiple choices to pick from.
+
+function startQuiz() {
+    welcomeSection.style.display = "none";
+    questionSection.style.display = "block";
+    currentQuestion = 0;
+    startTimer();
+    askQuestions();
+}
+
+// this function displays the first question object within the arrayOfQuestions array.
+// this loops through each one of the questions for the length of the arrayOfQuestions array.
+
+function askQuestions() {
+    if (currentQuestion === arrayOfQuestions.length) {
+        questionSection.style.display = "none";
+        initialsSection.style.display = "block";
+    }
+    presentQuestion.textContent = arrayOfQuestions[currentQuestion].quizQuestion;
+    firstChoice.textContent = arrayOfQuestions[currentQuestion].potentialAnswers[0];
+    secondChoice.textContent = arrayOfQuestions[currentQuestion].potentialAnswers[1];
+    thirdChoice.textContent = arrayOfQuestions[currentQuestion].potentialAnswers[2];
+    fourthChoice.textContent = arrayOfQuestions[currentQuestion].potentialAnswers[3];
+}
+
+// user selects their choice via a button, logs if the answer is correct or wrong in the console 
+// calls back askQuestions function to advance to the next question after a selection is made
+
+choiceButtons.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (event.target.matches("button")) {
+        if (event.target.textContent === arrayOfQuestions[currentQuestion].correctAnswer) {
+            currentQuestion++;
+            console.log("correct");
+        }
+        else {
+            currentQuestion++;
+            timeRemaining = timeRemaining - 10;
+            console.log("wrong");
+        }
+        askQuestions();
+    }
+});
